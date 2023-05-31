97c5102bb360d8c041543839f3eebb0b2838fd2f
Marcelo Adade <marceloadade@gmail.com>
2023-05-31 20:27:57 -0300

    Create configure_Log_Ship_database.sql

---
+++ Keeping Your SQL Server Always On/scripts/Log Shipping/configure_Log_Ship_database.sql
@@ -0,0 +1,194 @@
+
+-- Execute the following statements at the Primary to configure Log Shipping 
+-- for the database [POMBOSERVER\LAB2019].[AdventureWorksLarge],
+-- The script needs to be run at the Primary in the context of the [msdb] database.  
+------------------------------------------------------------------------------------- 
+-- Adding the Log Shipping configuration 
+
+-- ****** Begin: Script to be run at Primary: [POMBOSERVER\LAB2019] ******
+
+
+DECLARE @LS_BackupJobId	AS uniqueidentifier 
+DECLARE @LS_PrimaryId	AS uniqueidentifier 
+DECLARE @SP_Add_RetCode	As int 
+
+
+EXEC @SP_Add_RetCode = master.dbo.sp_add_log_shipping_primary_database 
+		@database = N'AdventureWorksLarge' 
+		,@backup_directory = N'D:\temp' 
+		,@backup_share = N'\\POMBOSERVER\temp' 
+		,@backup_job_name = N'LSBackup_AdventureWorksLarge' 
+		,@backup_retention_period = 4320
+		,@backup_compression = 1
+		,@backup_threshold = 60 
+		,@threshold_alert_enabled = 1
+		,@history_retention_period = 5760 
+		,@backup_job_id = @LS_BackupJobId OUTPUT 
+		,@primary_id = @LS_PrimaryId OUTPUT 
+		,@overwrite = 1 
+
+
+IF (@@ERROR = 0 AND @SP_Add_RetCode = 0) 
+BEGIN 
+
+DECLARE @LS_BackUpScheduleUID	As uniqueidentifier 
+DECLARE @LS_BackUpScheduleID	AS int 
+
+
+EXEC msdb.dbo.sp_add_schedule 
+		@schedule_name =N'LSBackupSchedule_POMBOSERVER\LAB20191' 
+		,@enabled = 1 
+		,@freq_type = 4 
+		,@freq_interval = 1 
+		,@freq_subday_type = 4 
+		,@freq_subday_interval = 15 
+		,@freq_recurrence_factor = 0 
+		,@active_start_date = 20230531 
+		,@active_end_date = 99991231 
+		,@active_start_time = 0 
+		,@active_end_time = 235900 
+		,@schedule_uid = @LS_BackUpScheduleUID OUTPUT 
+		,@schedule_id = @LS_BackUpScheduleID OUTPUT 
+
+EXEC msdb.dbo.sp_attach_schedule 
+		@job_id = @LS_BackupJobId 
+		,@schedule_id = @LS_BackUpScheduleID  
+
+EXEC msdb.dbo.sp_update_job 
+		@job_id = @LS_BackupJobId 
+		,@enabled = 1 
+
+
+END 
+
+
+EXEC master.dbo.sp_add_log_shipping_alert_job 
+
+EXEC master.dbo.sp_add_log_shipping_primary_secondary 
+		@primary_database = N'AdventureWorksLarge' 
+		,@secondary_server = N'POMBOSERVER\LAB2022CORE' 
+		,@secondary_database = N'AdventureWorksLarge' 
+		,@overwrite = 1 
+
+-- ****** End: Script to be run at Primary: [POMBOSERVER\LAB2019]  ******
+
+
+-- Execute the following statements at the Secondary to configure Log Shipping 
+-- for the database [POMBOSERVER\LAB2022CORE].[AdventureWorksLarge],
+-- the script needs to be run at the Secondary in the context of the [msdb] database. 
+------------------------------------------------------------------------------------- 
+-- Adding the Log Shipping configuration 
+
+-- ****** Begin: Script to be run at Secondary: [POMBOSERVER\LAB2022CORE] ******
+
+
+DECLARE @LS_Secondary__CopyJobId	AS uniqueidentifier 
+DECLARE @LS_Secondary__RestoreJobId	AS uniqueidentifier 
+DECLARE @LS_Secondary__SecondaryId	AS uniqueidentifier 
+DECLARE @LS_Add_RetCode	As int 
+
+
+EXEC @LS_Add_RetCode = master.dbo.sp_add_log_shipping_secondary_primary 
+		@primary_server = N'POMBOSERVER\LAB2019' 
+		,@primary_database = N'AdventureWorksLarge' 
+		,@backup_source_directory = N'\\POMBOSERVER\temp' 
+		,@backup_destination_directory = N'D:\temp' 
+		,@copy_job_name = N'LSCopy_POMBOSERVER\LAB2019_AdventureWorksLarge' 
+		,@restore_job_name = N'LSRestore_POMBOSERVER\LAB2019_AdventureWorksLarge' 
+		,@file_retention_period = 4320 
+		,@overwrite = 1 
+		,@copy_job_id = @LS_Secondary__CopyJobId OUTPUT 
+		,@restore_job_id = @LS_Secondary__RestoreJobId OUTPUT 
+		,@secondary_id = @LS_Secondary__SecondaryId OUTPUT 
+
+IF (@@ERROR = 0 AND @LS_Add_RetCode = 0) 
+BEGIN 
+
+DECLARE @LS_SecondaryCopyJobScheduleUID	As uniqueidentifier 
+DECLARE @LS_SecondaryCopyJobScheduleID	AS int 
+
+
+EXEC msdb.dbo.sp_add_schedule 
+		@schedule_name =N'DefaultCopyJobSchedule' 
+		,@enabled = 1 
+		,@freq_type = 4 
+		,@freq_interval = 1 
+		,@freq_subday_type = 4 
+		,@freq_subday_interval = 15 
+		,@freq_recurrence_factor = 0 
+		,@active_start_date = 20230531 
+		,@active_end_date = 99991231 
+		,@active_start_time = 0 
+		,@active_end_time = 235900 
+		,@schedule_uid = @LS_SecondaryCopyJobScheduleUID OUTPUT 
+		,@schedule_id = @LS_SecondaryCopyJobScheduleID OUTPUT 
+
+EXEC msdb.dbo.sp_attach_schedule 
+		@job_id = @LS_Secondary__CopyJobId 
+		,@schedule_id = @LS_SecondaryCopyJobScheduleID  
+
+DECLARE @LS_SecondaryRestoreJobScheduleUID	As uniqueidentifier 
+DECLARE @LS_SecondaryRestoreJobScheduleID	AS int 
+
+
+EXEC msdb.dbo.sp_add_schedule 
+		@schedule_name =N'DefaultRestoreJobSchedule' 
+		,@enabled = 1 
+		,@freq_type = 4 
+		,@freq_interval = 1 
+		,@freq_subday_type = 4 
+		,@freq_subday_interval = 15 
+		,@freq_recurrence_factor = 0 
+		,@active_start_date = 20230531 
+		,@active_end_date = 99991231 
+		,@active_start_time = 0 
+		,@active_end_time = 235900 
+		,@schedule_uid = @LS_SecondaryRestoreJobScheduleUID OUTPUT 
+		,@schedule_id = @LS_SecondaryRestoreJobScheduleID OUTPUT 
+
+EXEC msdb.dbo.sp_attach_schedule 
+		@job_id = @LS_Secondary__RestoreJobId 
+		,@schedule_id = @LS_SecondaryRestoreJobScheduleID  
+
+
+END 
+
+
+DECLARE @LS_Add_RetCode2	As int 
+
+
+IF (@@ERROR = 0 AND @LS_Add_RetCode = 0) 
+BEGIN 
+
+EXEC @LS_Add_RetCode2 = master.dbo.sp_add_log_shipping_secondary_database 
+		@secondary_database = N'AdventureWorksLarge' 
+		,@primary_server = N'POMBOSERVER\LAB2019' 
+		,@primary_database = N'AdventureWorksLarge' 
+		,@restore_delay = 0 
+		,@restore_mode = 0 
+		,@disconnect_users	= 0 
+		,@restore_threshold = 45   
+		,@threshold_alert_enabled = 1 
+		,@history_retention_period	= 5760 
+		,@overwrite = 1 
+
+END 
+
+
+IF (@@error = 0 AND @LS_Add_RetCode = 0) 
+BEGIN 
+
+EXEC msdb.dbo.sp_update_job 
+		@job_id = @LS_Secondary__CopyJobId 
+		,@enabled = 1 
+
+EXEC msdb.dbo.sp_update_job 
+		@job_id = @LS_Secondary__RestoreJobId 
+		,@enabled = 1 
+
+END 
+
+
+-- ****** End: Script to be run at Secondary: [POMBOSERVER\LAB2022CORE] ******
+
+
